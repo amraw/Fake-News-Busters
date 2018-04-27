facc081cf1913f61fddea59e99f1e76238a2ea91
amraw <amitrawat10@gmail.com>
2018-04-27 09:54:28 -0500

    Some changes

--- Simple_feedforward_with_bag_of_words.py
+++ Simple_feedforward_with_bag_of_words.py
@@ -24,7 +24,7 @@ def feed_forward_model(numb_epocs):
     data = load_data()
 
     # Loading train data from files
-    data.set_path(path='./fnc-1-master')
+    data.set_path(path='fnc-1-master')
     train_stance_data = data.get_headline_body_stance()
     train_bodies_data = data.get_body_id_text()
     train_headlines, train_bodies, train_stances = data.get_mapped_id_body(train_stance_data, train_bodies_data)
